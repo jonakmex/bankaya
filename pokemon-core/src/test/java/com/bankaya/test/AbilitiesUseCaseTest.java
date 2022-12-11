22ffaf458661a4406a5d41bfbdd20468c7225aa2
JONATHAN GOMEZ <jonathangomez@JONATHANs-MacBook-Air.local>
2022-12-10 21:26:45 -0600

    Test environment setup

---
+++ pokemon-core/src/test/java/com/bankaya/test/AbilitiesUseCaseTest.java
@@ -0,0 +1,12 @@
+package com.bankaya.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AbilitiesUseCaseTest {
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
